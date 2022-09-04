eaee819f94b065648da1d47798c5d6dad82e7af7
Lautaro Barrera <lautarobarrera12@gmail.com>
2022-09-04 20:17:51 -0300

    estilos de estado de completado creados

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import './Form.css';
+import iconComplete from './icon-complete.svg';
+import { Button } from '../Button';
 
 function Form({children}) {
+    const [completedForm, setCompletedForm] = React.useState(true);
+    if (completedForm) {
+        return(
+            <div className="completed-container">
+                <img src={iconComplete}/>
+                <h2 className="completed-title">¡Gracias!</h2>
+                <p className="completed-text">Hemos añadido los datos de su tarjeta</p>
+                <Button>Continuar</Button>
+            </div>
+        );
+    }
     return(
         <form className="form">
             {children}
